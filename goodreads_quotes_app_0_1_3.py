39f4fb99b5f935855498c327ad21f57ebd36a6e6
gero zayas <gerozayas@gmail.com>
2023-07-03 09:37:01 +0200

    Add emojis Add fix me parts

--- goodreads_quotes_app_0_1_3.py
+++ goodreads_quotes_app_0_1_3.py
@@ -19,9 +19,8 @@ def scrape_quotes(page_url):
     authors = []
     books = []
     page_num = 1
-    
-    
-    # This code block is responsible for scraping the quotes, authors, and books 
+
+    # This code block is responsible for scraping the quotes, authors, and books
     # from the Goodreads author quotes page.
     while page_num <= 25:
         response = requests.get(f"{page_url}?page={page_num}")
@@ -50,53 +49,60 @@ def scrape_quotes(page_url):
 
 
 def main():
-    
     #############################################################
     # SIDE BAR: Suggested Quotes
     #############################################################
 
     st.sidebar.title("Suggested Quotes")
     # TODO: Click on suggested quotes and automatically scrape them
-    
+
     # DICTIONARY OF SUGGESTED QUOTES and their URLs
     suggested_quotes = {
         "Jordan B. Peterson": "https://www.goodreads.com/author/quotes/282885.Jordan_B_Peterson",
         "Isaac Asimov": "https://www.goodreads.com/author/quotes/16667.Isaac_Asimov",
         "Matthew Syed": "https://www.goodreads.com/author/quotes/3414480.Matthew_Syed",
     }
-    for author, url in suggested_quotes.items():
-        # st.sidebar.markdown(f"Goodreads: {author}")
-        st.sidebar.markdown(f"[{author}]({url})\n---")
-        # st.sidebar.write("---")
-        
+    help_msg = "Get the quotes from this author"
+    for author in suggested_quotes:
+        st.sidebar.button(f"{author} 😊", key=author, help=help_msg)
+
     #############################################################
     # MAIN PAGE: Goodreads Author Quotes Scraper
     #############################################################
 
-    st.title("Goodreads Author Quotes Scraper")
+    st.title("Goodreads Author Quotes Scraper 😄")
     st.write("Enter the Goodreads author quotes page URL:")
 
     # USER INPUT: Page URL ######################################
-    page_url = st.text_input("Page URL", "")
+
+    # If CLICK on SIde Bar suggested quotes, then automatically scrape them
+    # if st.sidebar.button("Jordan B. Peterson", key="Jordan B. Peterson"):
+    #     page_url = suggested_quotes["Jordan B. Peterson"]
+
+    # FIXME: When it is just quotes, and there's no book, don't show the book
+    
+    page_url = st.text_input("Page URL", "https://www.goodreads.com/quotes")
     if st.button("Scrape Quotes"):
         if page_url:
             quotes, authors, books = scrape_quotes(page_url)
-            st.write(f"Total Quotes: {len(quotes)}")
+            st.write(f"Total Quotes: {len(quotes)} 📚", unsafe_allow_html=True)
             separator = "\n---\n"
 
             for i in range(len(quotes)):
-                st.markdown(f"### {quotes[i]}")
+                st.markdown(f"### 💡 {quotes[i]}")
                 st.markdown(
                     f"<h5 style='color: darkred;display: inline;'>{authors[i]}</h5>",
                     unsafe_allow_html=True,
                 )
+                # FIXME: Add a space between the emoji and the book title
                 st.markdown(
-                    f"<h6 style='color: #4a008f;display: inline;'>{books[i]}</h6>",
+                    f"<h6 style='color: #4a008f;display: inline;'> 📖{books[i]}</h6>",
                     unsafe_allow_html=True,
                 )
                 st.markdown("<br>", unsafe_allow_html=True)
                 if (i + 1) % 15 == 0 and i != len(quotes) - 1:
                     st.markdown(separator)
+
         else:
             st.write("Please enter a valid page URL.")
 
